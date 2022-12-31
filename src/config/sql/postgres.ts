d6fdb5a0fa55af6d7e80a955e9b24e813518fde5
franrodriguez1993 <francojmprogramacion@gmail.com>
2022-12-31 15:28:26 -0300

    postgres remote

--- src/config/sql/postgres.ts
+++ src/config/sql/postgres.ts
@@ -1,7 +1,17 @@
 //La recomendación de la documentación dice que es mejor importarlo con mayúscula.
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("commerceDB", "postgres", "147258", {
-  host: "localhost",
-  dialect: "postgres",
-});
+export const sequelize = new Sequelize(
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USERNAME,
+  process.env.POSTGRES_PASSWORD,
+  {
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres",
+  }
+);
+
+// export const sequelize = new Sequelize("commerceDB", "postgres", "147258", {
+//   host: "localhost",
+//   dialect: "postgres",
+// });
